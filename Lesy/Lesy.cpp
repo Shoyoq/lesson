c19b13c3fadf29261a2a77cc064269a79003eb76
student4 <stud4@STUDENT4>
2025-04-16 20:09:12 +0700

    dz

--- Lesy/Lesy.cpp
+++ Lesy/Lesy.cpp
@@ -9,13 +9,13 @@
 
 
 
-
 void game() {
 	
+	std::cout << "GAME START\n";
 
 	int b;
 
-	for (int i = 0; i < 10; i++) {
+	for (int i = 0; i < 1;) {
 
 	int value = 1 + rand() % 3;
 		std::cin >> b;
@@ -23,7 +23,7 @@ void game() {
 		if (b == 1 && value == 3 || b == 2 && value == 1 || b == 3 && value == 1 ) {
 
 			std::cout << "you win\n";
-
+			i++;
 		}
 		else if (b == value) {
 		
@@ -39,16 +39,50 @@ void game() {
 
 
 }
-//gerfgdfgdfghdhdfh
 
+
+void Begin() {
+
+	std::cout << "Hi, sosal?\n";
+
+	std::string y;
+	std::cin >> y;
+
+	if (y == "da") {
+
+		std::cout << "me too\n";
+	}
+	else {
+
+	game();
+	}
+
+ }
 
 
 int main()
 {
-	setlocale(LC_ALL, "");
+	//setlocale(LC_ALL, "");
 	srand(time(NULL));
 
-	game();
+	int a[3] = {4, 5, -6};
+	char x[4] = {'t','e','s', 't'};
+
+	Begin();
+
+
+	/*float b = 2.9;
+
+	for (int i = 0; i < 4; i++) {
+
+		std::cout << x[i] << " ";
+	}*/
+
+
+	//std::cout << a << " " << b << " " << x[1];
+
+
+	//game();
 
 }
 
