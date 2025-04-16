d1ba16136b8296eb674f40d53448f4082380bee3
student4 <stud4@STUDENT4>
2025-04-16 19:21:23 +0700

    Add project files.

---
+++ Lesy/Lesy.cpp
@@ -0,0 +1,54 @@
+
+
+
+#include <iostream>
+#include <ctime>
+#include <string>
+
+
+
+
+
+
+void game() {
+	
+
+	int b;
+
+	for (int i = 0; i < 10; i++) {
+
+	int value = 1 + rand() % 3;
+		std::cin >> b;
+
+		if (b == 1 && value == 3 || b == 2 && value == 1 || b == 3 && value == 1 ) {
+
+			std::cout << "you win\n";
+
+		}
+		else if (b == value) {
+		
+			std::cout << "ничья\n";
+
+		}
+		else
+		{
+			std::cout << "you lose\n";
+		}
+
+	}
+
+
+}
+
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	srand(time(NULL));
+
+	game();
+
+}
+
