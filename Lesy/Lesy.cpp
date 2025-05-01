7d4f33ce5409a7507bfb0788051e6f663c40312a
student4 <stud4@STUDENT4>
2025-05-01 19:23:05 +0700

    tчтобы запускалось заново при неправильном вводе портала. ввести переменную, чтобы игра работала до момента смерти

--- Lesy/Lesy.cpp
+++ Lesy/Lesy.cpp
@@ -1,19 +1,62 @@
 
 #include <iostream>
 
-#include <ctime>
-#include <string>
+//#include <ctime>
+//#include <string>
 #include <thread>
 #include <chrono>
+#include <vector>
 
 using namespace std;
 
-void typeText(const string& text, int delay_ms = 50) {
-	for (char c : text) {
-		cout << c << flush;
+
+//class tring {
+//	public:
+//
+//	int size = 4;
+//
+//	char *m = new char[size];
+//
+//	tring(int a) {
+//
+//		size = a;
+//	}
+//
+//	~tring() {
+//		delete[] m;
+//	}
+//};
+
+struct portal_ {
+
+	string name;
+	int target;
+
+};
+
+struct location_ {
+
+	string name;
+	vector<portal_> p;
+
+};
+
+struct info_ {
+
+	int current_loc = 0;
+
+};
+
+location_ loc[3];
+info_ player;
+
+void typeText(const string& text, int delay_ms) {
+	for (int i = 0; i < text.size(); i++) {
+		cout << text[i] << flush;
 		this_thread::sleep_for(chrono::milliseconds(delay_ms));
 	}
 	cout << endl;
+	
 }
 
 
@@ -24,8 +67,8 @@ void game() {
 	srand(time(NULL));
 
 
-	string choices[] = { "stone", "scissors", "paper" };
-	int num_choices = sizeof(choices) / sizeof(choices[0]);
+	string choices[3] = { "stone", "scissors", "paper" };
+	int size = sizeof(choices) / sizeof(choices[0]);
 	for (;;)
 	{
 		string user_choice;
@@ -33,9 +76,10 @@ void game() {
 		typeText(massage, 100);
 		cin >> user_choice;
 
-		int computer_index = rand() % num_choices;
+		int computer_index = rand() % 3;
 		string computer_choice = choices[computer_index];
-		cout << "computer choose:" << " " << computer_choice << endl;
+		cout << "computer choose:" << " "  << endl;
+		typeText(computer_choice, 100);
 
 		if (user_choice == computer_choice) {
 			cout << "draw\n";
@@ -54,6 +98,44 @@ void game() {
 	}
 }
 
+void eazy() {
+
+	string user;
+	string bot[3] = { "stone", "scissors", "paper" };
+	string b[3]{ "you win!!", "you lose!", "draw" };
+
+	while (true) {
+
+		string massage = "choose: stone, scissors, paper";
+		typeText(massage, 100);
+		int r = rand() % 3;
+
+
+		cin >> user;
+		typeText(bot[r], 100);
+
+		if (user == bot[r]) {
+			//cout << "draw\n";
+			typeText(b[2], 100);
+			cout << std::endl;
+		}
+		else if ((user == "stone" && bot[r] == "scissors") ||
+			(user == "scissors" && bot[r] == "paper") ||
+			(user == "paper" && bot[r] == "stone")) {
+
+			//cout << "you win!\n";
+			typeText(b[0], 100);
+			cout << std::endl;
+		}
+		else {
+
+			typeText(b[1], 100);
+			//cout << "you lose!\n";
+			cout << std::endl;
+		}
+		
+	}
+}
 
 void Begin() {
 
@@ -69,20 +151,60 @@ void Begin() {
 	if (y == "go")
 	{
 		cout << endl;
-		game();
+		//game();
+		eazy();
 	}
 	else {
 		cout << "ti durak";
 	}
 }
 
+int temp() {
+	int mas[] = {1, 2, 4,6, 4};
+	
+	int size = sizeof(mas) / sizeof(mas[0]);
+
+	return size;
+}
 
 int main()
 {
 	srand(time(NULL));
 
+	loc[0].name = "HUB";
+	loc[1].name = "DOM";
+	loc[2].name = "TRASH";
+	loc[0].p.push_back({ "DOOR", 1 });
+	loc[0].p.push_back({ "TRASH", 2 });
+
 	int a[3] = { 4, 5, -6 };
 	char x[4] = { 't','e','s', 't' };
+	string x1 = "test";
+	//Begin();
+	int o = 4;
+	/*tring p(3);*/
 
-	Begin();
+	string ch;
+
+	cin >> ch;
+
+
+	if (ch == "go") {
+
+		for (int i = 0; i < loc[player.current_loc].p.size(); i++) {
+
+			cout << loc[player.current_loc].p[i].name << endl;
+		}
+
+		cin >> ch;
+
+		for (int i = 0; i < loc[player.current_loc].p.size(); i++) {
+
+			if (ch == loc[player.current_loc].p[i].name) {
+
+				player.current_loc = loc[player.current_loc].p[i].target;
+				cout << "you: " << loc[player.current_loc].name << endl;
+			}
+		}
+	}
 }
