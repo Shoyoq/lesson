0fea9f6cc23765a1fa5dd55485b99708d859631c
Shoyoq <oklearlesya@gmail.com>
2025-05-01 16:11:56 +0700

    test

--- Lesy/Lesy.cpp
+++ Lesy/Lesy.cpp
@@ -1,88 +1,88 @@
 
-
-
 #include <iostream>
+
 #include <ctime>
 #include <string>
+#include <thread>
+#include <chrono>
 
+using namespace std;
+
+void typeText(const string& text, int delay_ms = 50) {
+	for (char c : text) {
+		cout << c << flush;
+		this_thread::sleep_for(chrono::milliseconds(delay_ms));
+	}
+	cout << endl;
+}
 
 
 
 
 void game() {
-	
-	std::cout << "GAME START\n";
 
-	int b;
+	srand(time(NULL));
 
-	for (int i = 0; i < 1;) {
 
-	int value = 1 + rand() % 3;
-		std::cin >> b;
+	string choices[] = { "stone", "scissors", "paper" };
+	int num_choices = sizeof(choices) / sizeof(choices[0]);
+	for (;;)
+	{
+		string user_choice;
+		string massage = "choose: stone, scissors, paper";
+		typeText(massage, 100);
+		cin >> user_choice;
 
-		if (b == 1 && value == 3 || b == 2 && value == 1 || b == 3 && value == 1 ) {
+		int computer_index = rand() % num_choices;
+		string computer_choice = choices[computer_index];
+		cout << "computer choose:" << " " << computer_choice << endl;
 
-			std::cout << "you win\n";
-			i++;
+		if (user_choice == computer_choice) {
+			cout << "draw\n";
+			cout << std::endl;
 		}
-		else if (b == value) {
-		
-			std::cout << "ничья\n";
-
+		else if ((user_choice == "stone" && computer_choice == "scissors") ||
+			(user_choice == "scissors" && computer_choice == "paper") ||
+			(user_choice == "paper" && computer_choice == "stone")) {
+			cout << "you win!\n";
+			cout << std::endl;
 		}
-		else
-		{
-			std::cout << "you lose\n";
+		else {
+			cout << "you lose!\n";
+			cout << std::endl;
 		}
-
 	}
-
-
 }
 
 
 void Begin() {
 
-	std::cout << "Hi, sosal?\n";
+
+
+
+	string massage = ("start game\n" "enter GO\n");
+	typeText(massage, 100);
 
 	std::string y;
 	std::cin >> y;
 
-	if (y == "da") {
-
-		std::cout << "me too\n";
+	if (y == "go")
+	{
+		cout << endl;
+		game();
 	}
 	else {
-
-	game();
+		cout << "ti durak";
 	}
-
- }
+}
 
 
 int main()
 {
-	//setlocale(LC_ALL, "");
 	srand(time(NULL));
 
-	int a[3] = {4, 5, -6};
-	char x[4] = {'t','e','s', 't'};
+	int a[3] = { 4, 5, -6 };
+	char x[4] = { 't','e','s', 't' };
 
 	Begin();
-
-
-	/*float b = 2.9;
-
-	for (int i = 0; i < 4; i++) {
-
-		std::cout << x[i] << " ";
-	}*/
-
-
-	//std::cout << a << " " << b << " " << x[1];
-
-
-	//game();
-
 }
-
